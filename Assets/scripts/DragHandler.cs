3687df14c728da35512690ab31de8cc0a6db4b4b
Ankur Jain <ankurjainaj1234@gmail.com>
2019-06-14 15:07:45 +0530

    first commit

---
+++ Assets/scripts/DragHandler.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using UnityEngine.EventSystems;
+using GoogleARCore.Examples.Common;
+
+public class DragHandler : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+    public GameObject prefab;
+    GameObject hoverPrefab;
+    DetectedPlaneGenerator detectedPlaneGenerator;
+    List<TrackableHit> trackableHitList = new List<TrackableHit>();
+
+    void Start()
+    {
+        hoverPrefab = Instantiate(prefab);
+        AdjustPrefabAlpha();
+        hoverPrefab.SetActive(false);
+        detectedPlaneGenerator = FindObjectOfType<DetectedPlaneGenerator>();
+    }
+
+    void AdjustPrefabAlpha()
+    {
+        MeshRenderer[] meshRenderers = hoverPrefab.GetComponentsInChildren<MeshRenderer>();
+        for (int i = 0; i < meshRenderers.Length; i++)
+        {
+            Material mat = meshRenderers[i].material;
+            meshRenderers[i].material.color = new Color(mat.renderQueue, mat.color.g, mat.color.b);
+        }
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Frame.RaycastAll(ray.origin, ray.direction, trackableHitList, Mathf.Infinity, TrackableHitFlags.Default);
+        if(trackableHitList !=null && trackableHitList.Count>0)
+        {
+            int terrainCollderQuadIndex = GetTerrainColliderQuadIndex(trackableHitList);
+            if(terrainCollderQuadIndex!=-1)
+            {
+                hoverPrefab.transform.position = trackableHitList[terrainCollderQuadIndex].Pose.position;
+                hoverPrefab.SetActive(true);
+            }
+            else
+            {
+                hoverPrefab.SetActive(false);
+            }
+        }
+    }
+    int GetTerrainColliderQuadIndex(List<TrackableHit> hits)
+    {
+        for(int i=0;i<hits.Count;i++)
+        {
+            if(hits[i].Trackable is DetectedPlane)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if(hoverPrefab.activeSelf)
+        {
+            Instantiate(prefab, hoverPrefab.transform.position, Quaternion.identity);
+            detectedPlaneGenerator.DisablePlanes();
+        }
+        hoverPrefab.SetActive(false);
+    }
+}
---
+++ Assets/scripts/ObjectSelection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean;
+using Lean.Touch;
+
+
+public class ObjectSelection : MonoBehaviour
+{
+    RaycastHit hit;
+
+    public void deselectAll()
+    {
+        foreach(GameObject g in GameObject.FindGameObjectsWithTag("solar"))
+        {
+            g.GetComponent<LeanTranslate>().CanTranslate = false;
+            g.GetComponent<LeanRotateCustomAxis>().CanRotate = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit))
+            {
+                if(hit.collider.gameObject.tag=="solar")
+                {
+                    deselectAll();
+                    hit.collider.gameObject.GetComponent<LeanTranslate>().CanTranslate = true;
+                    hit.collider.gameObject.GetComponent<LeanRotateCustomAxis>().CanRotate = true;
+
+                }
+            }
+        }
+    }
+}
